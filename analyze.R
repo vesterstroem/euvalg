fb4d047033f4126b387939db1c585813ae63acb3
Jakob Vesterstrøm <jakobvesterstroem@gmail.com>
2024-06-12 01:12:29 +0200

    extended analyze script

--- analyze.R
+++ analyze.R
@@ -6,27 +6,35 @@ args = commandArgs(trailingOnly=TRUE)
 dat <- read.csv("AllCandidates.complete.annotated.flat.csv")
 questions <- read.csv("questions.csv", header=FALSE, sep=";")
 
-if (length(args) > 0)
-{
-  # subset of questions based on topic
+if (length(args) == 0) {
+  topic <- ''
+} else {
   topic <- args[1]
-  topicIds <- questions[questions[, 2] %like% topic,c(1)]
-  topics <- questions[questions[, 2] %like% topic,c(2)]
-  df2 <- as.vector(as.matrix(topics))
-  uniqueTopics <- toString(unique(df2))
-  print(paste("Search: ", topic))
-  print(paste("Topics found: ", uniqueTopics))
-  print(paste("Questions found: ", length(df2)))
-  if (length(df2) == 0)
-  {
-	allTopics <- toString(unique(as.vector(as.matrix(questions[,c(2)]))))
-	print(paste("Possible topics: ", allTopics))
-	quit()
-  }
-
-  # reduce dataset based on selected topics
-  dat <- dat[,c(1,2, 2+ topicIds)]
 }
+
+# subset of questions based on topic
+topicIds <- questions[questions[, 2] %like% topic,c(1)]
+topics <- questions[questions[, 2] %like% topic,c(2)]
+
+topicsList <- as.vector(as.matrix(topics))
+uniqueTopics <- toString(unique(topicsList))
+
+print(paste("Query: ", topic))
+print(paste("Questions found: ", length(topicsList)))
+print("Topics found: ")
+print(paste("  ", uniqueTopics))
+
+allTopics <- as.vector(as.matrix(questions[,c(2)]))
+print(paste("All questions: ", length(allTopics)))
+print("All topics: ")
+print(paste("  ", toString(unique(allTopics))))
+
+if (length(topicsList) == 0) {
+	quit()
+}
+
+# reduce dataset based on selected topics
+dat <- dat[,c(1,2, 2+ topicIds)]
 
 # raw data without metadata
 x <- as.matrix(dat[, c(-1,-2)])
@@ -34,11 +42,10 @@ x <- as.matrix(dat[, c(-1,-2)])
 rownames(x) <- dat[,c(2)]
 manhattan_distance <- function (v1, v2) sum(abs(v1 - v2))
 res <- usedist::dist_make(x, manhattan_distance)
-
 m <- as.matrix(res)
 
 # add parti column to result
 joined <- cbind(Parti = dat$Parti, m)
 
-finalResult <- joined[order(joined[, 2]), c(1,2)]
+finalResult <- joined[order(as.numeric(joined[, 2])), c(1,2)]
 finalResult
